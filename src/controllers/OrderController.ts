f5ffaebe572d0d7fd10c5d685526f8e4e9996285
zhengsq23 <2270833979@qq.com>
2024-04-22 23:48:43 +0800

    Get customer orders API

--- src/controllers/OrderController.ts
+++ src/controllers/OrderController.ts
@@ -7,18 +7,18 @@ const STRIPE = new Stripe(process.env.STRIPE_API_KEY as string);
 const FRONTEND_URL = process.env.FRONTEND_URL as string;
 const STRIPE_ENDPOINT_SECRET = process.env.STRIPE_WEBHOOK_SECRET as string;
 
-// const getMyOrders = async (req: Request, res: Response) => {
-//   try {
-//     const orders = await Order.find({ user: req.userId })
-//       .populate("restaurant")
-//       .populate("user");
+const getMyOrders = async (req: Request, res: Response) => {
+  try {
+    const orders = await Order.find({ user: req.userId })
+      .populate("restaurant")
+      .populate("user");
 
-//     res.json(orders);
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ message: "something went wrong" });
-//   }
-// };
+    res.json(orders);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "something went wrong" });
+  }
+};
 
 type CheckoutSessionRequest = {
   cartItems: {
@@ -172,7 +172,7 @@ const createSession = async (
 };
 
 export default {
-  // getMyOrders,
+  getMyOrders,
   createCheckoutSession,
   stripeWebhookHandler,
 };
